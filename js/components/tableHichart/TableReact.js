06286ac2c74d4c1978a3b45c6550c925c2148e4c
Yurj7 <yugobli@gmail.com>
2017-09-01 02:42:15 +0300

    Added react-Hichart

--- js/components/tableHichart/TableReact.js
+++ js/components/tableHichart/TableReact.js
@@ -1,47 +1,16 @@
 import React, {Component} from 'react';
 import {repositories} from '../../mocks';
 import PropTypes from 'prop-types'
+import Charts from './../Charts';
 
 import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
 } from 'material-ui/Table';
-
-import styles from './tableStyle.scss';
-
-// class RepRaws extends Component {
-//     static propTypes = {
-//         value: PropTypes.shape({
-//             id: PropTypes.string.isRequired,
-//             name: PropTypes.string.isRequired,
-//             description: PropTypes.string.isRequired,
-//             stargazers_count: PropTypes.string.isRequired,
-//             watchers_count: PropTypes.string.isRequired,
-//             forks: PropTypes.string.isRequired,
-//             open_issues: PropTypes.string.isRequired,
-//
-//         }).isRequired,
-//         deleteItem: PropTypes.func
-//     };
-//
-//     render () {
-//         return (
-//             <TableRow key ={this.props.i} value={this.props.item}>
-//                 <TableRowColumn> {this.props.value.id} </TableRowColumn>
-//                 <TableRowColumn> {this.props.value.name} </TableRowColumn>
-//                 <TableRowColumn> {this.props.value.description} </TableRowColumn>
-//                 <TableRowColumn>{this.props.value.stargazers_count}</TableRowColumn>
-//                 <TableRowColumn>{this.props.value.watchers_count}</TableRowColumn>
-//                 <TableRowColumn>{this.props.value.forks}</TableRowColumn>
-//                 <TableRowColumn>{this.props.value.open_issues}</TableRowColumn>
-//             </TableRow>
-//         );
-//     }
-// }
 
 
 export default class TableWithData extends Component {
@@ -56,13 +25,39 @@ export default class TableWithData extends Component {
             stargazers_count: '',
             watchers_count: '',
             forks: '',
-            open_issues: ''
-        };
+            open_issues: '',
+            chartCollections: [],
+            selected: true
+        }
+        this.onRowSelected = this.onRowSelected.bind(this);
+    }
+    onRowSelected(rows) {
+        console.log(rows)
+        rows.map( i => {
+            console.log(repositories.items)
+            let rowData = repositories.items[i]
+            console.log(rowData)
+            this.setState({selected: rowData})
+            this.state.chartCollections.push({
+                name: rowData.name,
+                data: [
+                    rowData.stargazers_count,
+                    rowData.watchers,
+                    rowData.forks,
+                    rowData.open_issues
+                ]}
+
+            );
+        });
+
+        this.setState({ chartCollections: this.state.chartCollections});
+
     }
     render () {
         return (
             <div>
-                <Table multiSelectable={true}>
+                <Table multiSelectable={true}
+                       onRowSelection={this.onRowSelected}>
                     <TableHeader displaySelectAll={false}>
                         <TableRow>
                             <TableHeaderColumn>id</TableHeaderColumn>
@@ -77,43 +72,25 @@ export default class TableWithData extends Component {
                     <TableBody stripedRows={true}>
                         {/*{ this.state.repositories.items.map((item,i) => {if (i<5) return( <RepRaws key={i} value={item} />) })}*/}
                         {/*{ this.state.repositories.items.map(item => <RepRaws key={item.id} value={item}  />) }*/}
+                        {/*selected={this.state.repositories.items[i]!==-1}* --->Selected all items*/}
                         {this.state.repositories.items.map((item, i) =>
-                        {if (i<5) return(
-                            <TableRow key={i} value={item}>
-                                <TableRowColumn>{item.id}</TableRowColumn>
-                                <TableRowColumn>{item.name}</TableRowColumn>
-                                <TableRowColumn>{item.description}</TableRowColumn>
-                                <TableRowColumn>{item.stargazers_count}</TableRowColumn>
-                                <TableRowColumn>{item.watchers_count}</TableRowColumn>
-                                <TableRowColumn>{item.forks}</TableRowColumn>
-                                <TableRowColumn>{item.open_issues}</TableRowColumn>
-
-                            </TableRow>
-
-                        )}
+                            {if (i<5) return(
+                                <TableRow key={i} value={item} >
+                                    <TableRowColumn>{item.id}</TableRowColumn>
+                                    <TableRowColumn>{item.name}</TableRowColumn>
+                                    <TableRowColumn>{item.description}</TableRowColumn>
+                                    <TableRowColumn>{item.stargazers_count}</TableRowColumn>
+                                    <TableRowColumn>{item.watchers_count}</TableRowColumn>
+                                    <TableRowColumn>{item.forks}</TableRowColumn>
+                                    <TableRowColumn>{item.open_issues}</TableRowColumn>
+                                </TableRow>
+                            )}
                         )}
 
                     </TableBody>
                 </Table>
+                <Charts series={this.state.chartCollections} />
             </div>
         );
     }
 }
-
-
-// const TableHeaderContents = () => (
-//         <TableRow>
-//             <TableHeaderColumn>ID</TableHeaderColumn>
-//             <TableHeaderColumn>Name</TableHeaderColumn>
-//             <TableHeaderColumn>Description</TableHeaderColumn>
-//             <TableHeaderColumn>stargazers_count</TableHeaderColumn>
-//             <TableHeaderColumn>watchers</TableHeaderColumn>
-//             <TableHeaderColumn>folks</TableHeaderColumn>
-//         </TableRow>
-//
-//
-//         );
-
-
-
-
